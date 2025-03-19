a4ebd671651400f9c6be60beb597c53bf8c7b424
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-19 15:19:18 -0400

    Fixed compilation error
    Fixed compilation error. ALso got continuous generation of asteroid fields working, but numbers need to be tweaked.

--- Assets/Scripts/Asteroid/AsteroidFieldGeneration.cs
+++ Assets/Scripts/Asteroid/AsteroidFieldGeneration.cs
@@ -1,28 +1,67 @@
+using System.Collections;
 using UnityEngine;
 
 public class AsteroidFieldGeneration : MonoBehaviour
 {
+    [SerializeField] GameObject asteroidField;
+    [SerializeField] Transform generatorTransform;
     [SerializeField] Vector3 startPos;
     [SerializeField] Vector3 endPos;
     [SerializeField] int speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GenerateFieldParams();
+        StartCoroutine("ContinuousGeneration");
     }
 
     public void GenerateFieldParams()
     {
-        int dir = Random.Range(0, 3);
+        int dir = Random.Range(0, 4);
+        int x = 0;
+        int y = -500;
+        int z = 0;
         switch (dir)
-        int x = Random.Range()
+        {
+            case 0:
+                x = 1000;
+                z = Random.Range(-500, 500);
+                break;
+            case 1:
+                x = -1000;
+                z = Random.Range(-500, 500);
+                break;
+            case 2:
+                z = 1000;
+                x = Random.Range(-500, 500);
+                break;
+            case 3:
+                z = -1000;
+                x = Random.Range(-500, 500);
+                break;
+                y = Random.Range(-750, -250);
+        }
+        startPos = new Vector3(x, y, z);
+        y = y + 500;
+        endPos = new Vector3(-x, 500 - y, -z);
+        speed = Random.Range(5, 30);
     }
 
-    public void GenerateField()
+    private IEnumerator ContinuousGeneration()
+    {
+        GenerateField();
+        float time = 1000.0f / speed;
+        while (true) 
+        { 
+            yield return new WaitForSeconds(time);
+            GenerateField();
+        }
+    }
+
+    private void GenerateField()
+    {
+        GameObject field = Instantiate(asteroidField, generatorTransform);
+        field.transform.localPosition = startPos;
+        field.GetComponent<Asteroids>().InitializeField(startPos, endPos, speed);
+    }
 }
