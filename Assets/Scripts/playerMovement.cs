227e5d6eefab0cb33e1dc5b8df95b5c5fea1ed48
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-24 16:29:19 -0400

    Changes to player ship movement (Trent approved)
    Ship now rolls not yaws. Ship also uses the rigidbody and transform.forward for forward movement. Also removed the player prefab I added into the training scene and accidently included in the last push.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,62 +7,36 @@ public class playerMovement : MonoBehaviour
 {
     private float movementX;
     private float movementZ;
-    private float momentumX;
-    private float momentumZ;
-    public float angle = 10.0f;
+    public float angle = 90.0f;
     public float thrust = 5.0f;
-    public float speed = 10;
+    public float speed = 50;
     public Rigidbody rBdy;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Get player input
     void OnMove(InputValue movementValue) {
         UnityEngine.Vector2 movementVector = movementValue.Get<UnityEngine.Vector2>();
-        // Debug.Log(movementVector.x + ", " + movementVector.y);
-        movementX = movementVector.x;
-        movementZ = movementVector.y;
+        movementX = movementVector.y;
+        movementZ = movementVector.x;
     }
-    //Spacebar for "boost"
-    void OnJump() {
-        UnityEngine.Vector3 vector = new UnityEngine.Vector3(0.0f, 0.0f, thrust);
-        this.transform.forward = vector * speed * Time.deltaTime;
 
-    //     boost += 0.5f;
-    //     Vector3 vector = new Vector3(0.0f, 0.0f, boost);
-    //     rBdy.AddForce(vector * speed);
-    }
-    //C for "crouch"
-    // void OnCrouch() {
-    //     brake -= 0.5f;
-    //     Vector3 vector = new Vector3(0.0f, 0.0f, brake);
-    //     rBdy.AddForce(vector * speed);
-    // }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //Rotation inputs (i.e. input values 0 and 1 for the ai's input buffer)
-        if (movementX <= 1 && movementX >= -1) {
-            momentumX = movementX;
-            this.transform.RotateAround(this.transform.position, this.transform.up, momentumX * angle * Time.deltaTime);
-        }
-        if (movementZ <= 1 && movementZ >= -1) {
-            momentumZ = movementZ;
-            this.transform.RotateAround(this.transform.position, this.transform.right, momentumZ * angle * Time.deltaTime);
-        }
-        UnityEngine.Vector3 vector = new UnityEngine.Vector3(0.0f, 0.0f, thrust * speed * Time.deltaTime);
-        //Constant Thrust
-        // this.transform.forward = vector * speed * Time.deltaTime;
+        //Rotate and move the ship
+        this.transform.Rotate(new UnityEngine.Vector3(movementX, 0, -movementZ), angle * Time.deltaTime);
+        rBdy.linearVelocity = this.transform.forward * speed;
+       
+
+        // Experimental ai input code
+
+        /*
+        this.transform.forward = vector * speed * Time.deltaTime;
 
         // The neural net (i.e. policy) has generated output
-		// Vector3 controlSignal = Vector3.zero;
-        // vector = controlSignal.x;
-		// float angle = actionBuffers.ContinuousActions[1];
+		Vector3 controlSignal = Vector3.zero;
+        vector = controlSignal.x;
+		float angle = actionBuffers.ContinuousActions[1];
 
-		rBdy.AddRelativeForce(vector);
-
+		arBdy.AddRelativeForce(vector);
+        */
     }
 }
