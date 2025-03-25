94598c6e4d641ddfa16c698a7029fa623b344df9
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-25 13:00:48 -0400

    Added collision detection to laser script

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,7 +5,10 @@ namespace Assets.Scripts
 {
     public class Laser : MonoBehaviour
     {
+        [SerializeField] private GameObject laserExplosion;
+        [SerializeField] private GameObject shipExplosion;
         [SerializeField] private int speed;
+
         private Transform laserTransform;
         private Rigidbody rb;
 
@@ -18,6 +21,22 @@ namespace Assets.Scripts
         private void FixedUpdate()
         {
             rb.linearVelocity = laserTransform.forward * speed;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "agent")
+            {
+                Instantiate(laserExplosion, laserTransform.position, laserTransform.rotation);
+                //Instantiate(shipExplosion, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+                Destroy(this.gameObject);
+
+            }
+            else if (collision.gameObject.tag == "Obstacle")
+            {
+                Instantiate(laserExplosion, laserTransform.position, laserTransform.rotation);
+                Destroy(this.gameObject);
+            }
         }
 
         // Initializes rotation and velocity of a laser shot from a ship
