999cf78c7fccbd9751768ff02452dba8534c1464
nathansteenwyk-cu <nathansteenwyk@cedarville.edu>
2025-03-25 09:24:13 -0400

    More brain stuff
    fleshed out ShipBrain more, added config file, tried adding raycasts, added asteroid collision detection with negative reward

--- Assets/Scripts/ShipBrain.cs
+++ Assets/Scripts/ShipBrain.cs
@@ -6,6 +6,8 @@ using Unity.MLAgents.Actuators;
 public class ShipBrain : Agent
 {
     private agentMovement movementScript;
+    public float maxSteps = 50000f;
+    private float stepCount;
     void Start()
     {
         movementScript = GetComponent<agentMovement>();
@@ -13,12 +15,30 @@ public class ShipBrain : Agent
     }
     public override void OnEpisodeBegin()
     {
+        transform.position = new Vector3(0, 0, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            // Add a negative reward for hitting big space rock
+            AddReward(-1.0f);
+
+            // Log to console for debugging
+            Debug.Log("Asteroid Hit Penalty: " + -1.0f);
+        }
     }
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        AddReward(0.001f);
         float pitch = actionBuffers.ContinuousActions[0];
         float yaw = actionBuffers.ContinuousActions[1];
         movementScript.ApplyMovement(pitch, yaw);
+        if (stepCount >= maxSteps)
+        {
+            EndEpisode();
+        }
+        stepCount++;
     }
     public override void Heuristic(in ActionBuffers actionsOut)
     {
