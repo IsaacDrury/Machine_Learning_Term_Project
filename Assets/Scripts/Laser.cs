5e193dd5d84a2fb38cff3a6e676cea68cceba35f
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-25 12:34:59 -0400

    Laser script
    Laser go zoom

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Laser : MonoBehaviour
+    {
+        [SerializeField] private int speed;
+        private Transform laserTransform;
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            laserTransform = this.transform;
+            rb = this.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.linearVelocity = laserTransform.forward * speed;
+        }
+
+        // Initializes rotation and velocity of a laser shot from a ship
+        // Uses a reference to the ship's transform.
+        public void InitializeLazer(Transform parentTransform)
+        {
+            laserTransform.rotation = parentTransform.rotation;
+        }
+    }
+}
