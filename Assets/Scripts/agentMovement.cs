f2d157a4c67b4f4a9ddd2bce16fcb5cb2190efbf
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-04-15 16:46:30 -0400

    Fixed turret prefab
    Also added comments to cameracontrol script and made a change to collision detection in agent movement.

--- Assets/Scripts/agentMovement.cs
+++ Assets/Scripts/agentMovement.cs
@@ -15,15 +15,16 @@ public class agentMovement : MonoBehaviour
         health = this.gameObject.transform.GetChild(1).GetComponent<Health>();
     }
 
+    // Apply rotation and velocity
     public void ApplyMovement(float pitch, float yaw)
     {
         transform.Rotate(new Vector3(pitch, 0, -yaw), angle * Time.deltaTime);
-        // Set forward velocity
         rBdy.linearVelocity = transform.forward * speed;
     }
 
     public void OnCollisionEnter(Collision collision)
     {
+        // Collision with other ships
         if (collision.gameObject.tag == "Team 1" || collision.gameObject.tag == "Team 2")
         {
             Health otherHealth = collision.gameObject.transform.GetChild(1).GetComponent<Health>();
@@ -40,9 +41,17 @@ public class agentMovement : MonoBehaviour
                 health.ChangeHealth(24);
             }
         }
-        else if (collision.gameObject.tag == "Obstacle" && health.GetShipType() != "Cruiser")
+        // Collisions with asteroids and world borders
+        else if (collision.gameObject.tag == "Obstacle")
         {
-            health.ChangeHealth(6);
+            if (collision.gameObject.transform.parent.name == "WorldBorder")
+            {
+                health.ChangeHealth(200);
+            }
+            else if (health.GetShipType() != "Cruiser")
+            {
+                health.ChangeHealth(6);
+            }
         }
     }
 }
