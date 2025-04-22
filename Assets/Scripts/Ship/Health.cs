017d26a8f6c0b0689d05a6473ba3e4d84e3ed09f
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-04-22 14:13:11 -0400

    Created ship explosion
    Created explosion particle effect for ships that will now play when a ship is destroyed. The explosion also has audio and scales to a larger size for the bigger ships.

--- Assets/Scripts/Ship/Health.cs
+++ Assets/Scripts/Ship/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     [SerializeField] private GameObject ShipGenerator;
+    [SerializeField] private GameObject shipExplosion;
     [SerializeField] private int maxHealth;
     [SerializeField] private string type;
 
@@ -28,7 +29,17 @@ public class Health : MonoBehaviour
         slider.value = health;
         if (health <= 0)
         {
-            this.gameObject.transform.parent.gameObject.SetActive(false);
+            GameObject ship = this.gameObject.transform.parent.gameObject;
+            GameObject explosion = Instantiate(shipExplosion, ship.transform.position, ship.transform.rotation);
+            if (type == "Frigate")
+            {
+                explosion.transform.localScale = new Vector3(6, 6, 6);
+            }
+            else if (type == "Cruiser")
+            {
+                explosion.transform.localScale = new Vector3(36, 36, 36);
+            }
+            ship.SetActive(false);
             generatorScript.checkReset();
         }
     }
